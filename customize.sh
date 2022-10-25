e8ddfd86138898d9849f99cff882f7a13f8220d1
Ayaya <kazenosakura100@gmail.com>
2022-10-25 16:11:08 +0700

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,14 @@
+##########################################################################################
+#
+# MMT Extended Config Script
+#
+##########################################################################################
+
+# Set what you want to display when installing your module
+
+ui_print " " 
+ui_print "    for discussion = https://t.me/Allivee2nd   "
+ui_print "    for update = https://t.me/AlliveCh   "
+ui_print " "
+
+##########################################################################################
